4180a3d91a32293b42c58cb1de0bb05b5400e6c1
Diya Goyal <dgoyal6_be22@thapar.edu>
2025-03-25 09:58:26 +0530

    spring without boot

---
+++ src/main/java/com/diya/App.java
@@ -0,0 +1,19 @@
+package com.diya;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); // This is the Spring container that will manage the beans
+        dev obj = (dev) context.getBean("dev"); // This is the bean that we are trying to get from the container
+        // dev obj= new dev();
+         obj.build();
+    }
+}
---
+++ src/main/java/com/diya/dev.java
@@ -0,0 +1,7 @@
+package com.diya;
+
+public class dev {
+    public void build(){
+        System.out.println("Building the project");
+    }
+}
