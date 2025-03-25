e1c39fb89928e2f1aa79c4ab8c208f63a1fa14d0
Diya Goyal <dgoyal6_be22@thapar.edu>
2025-03-25 10:06:02 +0530

    spring without boot

--- src/main/java/com/diya/dev.java
+++ src/main/java/com/diya/dev.java
@@ -1,6 +1,10 @@
 package com.diya;
 
 public class dev {
+    public dev(){
+        System.out.println("dev constructor");
+    }
+
     public void build(){
         System.out.println("Building the project");
     }
---
+++ src/main/java/com/diya/laptop.java
@@ -0,0 +1,11 @@
+package com.diya;
+
+public class laptop {
+
+    public laptop(){
+        System.out.println("laptop constructor"); //this constructor doesnot need any object to be created
+    }
+    public void compile(){
+        System.out.println("Compiling the code"); // This is the method that we are going to call from the App.java
+    }
+}
